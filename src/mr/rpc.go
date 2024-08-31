0c63131680e08313a1b6a29b0bea26d34c43dc40
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2024-09-01 01:05:40 +0530

    added structs for RPC calls between workers and coordinator

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -23,7 +25,17 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
+type CoordinatorTaskArgs struct {
+	AssignedID int
+}
 
+type CoordinatorTaskReply struct {
+	TaskType          string
+	InputFile         string
+	AllTasksCompleted bool
+	NReduce           int
+	MapTaskID         int
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
